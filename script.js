78066b8f8dc59e3f5fae35841087cc819ac72425
Nikolay Antonovich <TesaloNick@gmail.com>
2021-02-13 16:55:51 +0300

    Task 4

--- script.js
+++ script.js
@@ -16,10 +16,25 @@ document.write('Получившаяся строка: ', date2_1, '<br>')
 
 // Задание 3
 
-document.write('<br>Задание 3. Преобразуйте эту дату в формат 31/12/2025.<br>')
+document.write('<br>Задание 3. Вырежете слово «учу» и слово «javascript» тремя разными способами.<br>')
 const str3 = 'Я учу javascript!'
-const strSlice = str3.slice(6, -1)
-const srtSubstr = str3.substr(6, 10)
-const strSubstring = str3.substring(6, 16)
-document.write('Извлекли слово jаvascript 3 способами: ', strSlice, ', ', srtSubstr, ', ', strSubstring, '.<br>')
+const strSlice_1 = str3.slice(2, 5)
+const srtSubstr_1 = str3.substr(2, 3)
+const strSubstring_1 = str3.substring(2, 5)
+const strSlice_2 = str3.slice(6, -1)
+const srtSubstr_2 = str3.substr(6, 10)
+const strSubstring_2 = str3.substring(6, 16)
+document.write('Извлекли слова учу и jаvascript 3 способами: ', strSlice_1, ', ', srtSubstr_1, ', ', strSubstring_1, ', ', strSlice_2, ', ', srtSubstr_2, ', ', strSubstring_2, '.<br>')
 
+// Задание 4
+
+document.write('<br>Задание 4. Найдите квадратный корень из суммы кубов его элементов.<br>')
+const arr4 = [4, 2, 5, 19, 13, 0, 10]
+let sumCube = 0, 
+    sumSqrt;
+for (let i=0; i < arr4.length; i++) {
+    sumCube += Math.pow(+arr4[i], 3);
+}
+// sumSqrt = Math.pow(sumCube, 0.5);
+sumSqrt = Math.sqrt(sumCube)
+document.write('Результат: ', sumCube, '.<br>')
